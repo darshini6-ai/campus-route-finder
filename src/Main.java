6f82775eb6925c02f59534cb3c0646ec6ff0ad0d
Darshini Sankar <darshinisankar6@gmail.com>
2026-08-27 04:47:47 +0530

    add initial Java source files

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Campus Route Finder");
+        System.out.println("Java project is working!");
+    }
+}
